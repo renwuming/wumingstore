d335673c9cbff484795e5793ec9c9637f64747e2
renwuming <renwuming@foxmail.com>
2017-07-27 14:57:13 +0800

    测试题的hash作为key来存储

--- router/test.js
+++ router/test.js
@@ -2,17 +2,22 @@ const Router = require("koa-router");
 const r = new Router();
 const mongodb = require("./lib/mongodb");
 const middleware = require("./middleware");
+const jsSHA = require("jssha");
 
 const COLLECTION = "test";
 
 r.post("/list", middleware.getSession(), async ( ctx ) => {
   const req = ctx.request.body;
   const query = {
-      _id: ctx.state.openid
-    };
+    _id: ctx.state.openid
+  };
+  let shaObj = new jsSHA("SHA-512", "TEXT");
+  shaObj.update(req.item);
+  const hash = shaObj.getHash("HEX");
+
   let list = (await mongodb.find(COLLECTION, query))[0];
-  list = list ? list.testlist : [];
-  list.push(req.item);
+  list = list ? list.testlist : {};
+  list[hash] = req.item;
 
   let res = await mongodb.update(COLLECTION, {
     _id: ctx.state.openid,
@@ -29,7 +34,7 @@ r.post("/getlist", middleware.getSession(), async ( ctx ) => {
       _id: ctx.state.openid
     };
   let list = (await mongodb.find(COLLECTION, query))[0];
-  list = list ? list.testlist : [];
+  list = list ? list.testlist : {};
 
   ctx.body = {
     testlist: list
