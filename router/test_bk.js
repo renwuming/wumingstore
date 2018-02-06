17201edae36416fcb93528b9ec9d8fae0eed0705
renwuming <renwuming@foxmail.com>
2018-02-06 15:20:08 +0800

    no message

--- router/test_bk.js
+++ router/test_bk.js
@@ -129,6 +129,7 @@ r.get("/friendtest/result", middleware.getSession(), async ( ctx ) => {
     if(!hash[player]) {
       hash[player] = await handleResult(item);
     }
+    if(Object.keys(hash).length>=1) break;
   }
 
   for(let k in hash) {
